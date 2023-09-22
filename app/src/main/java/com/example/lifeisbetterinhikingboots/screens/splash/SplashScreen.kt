a4df6e9efee0530aaf067300c1fe2cf80cbe65c5
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2023-09-22 20:20:28 +0300

    0.3  drawables added 📮

--- app/src/main/java/com/example/lifeisbetterinhikingboots/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/lifeisbetterinhikingboots/screens/splash/SplashScreen.kt
@@ -3,7 +3,9 @@ package com.example.lifeisbetterinhikingboots.screens.splash
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
+
 @Composable
 fun SplashScreen(){
     Text(text = "SplashScreen")
+
 }
